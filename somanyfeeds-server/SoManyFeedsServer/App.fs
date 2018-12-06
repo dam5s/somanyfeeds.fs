3d4cb66f03b0e8da0c89efb11f4561b2486d3d07
Damien Le Berrigaud <damien@itsdamo.com>
2018-12-06 16:57:17 -0600

    Some additional navigation.

--- somanyfeeds-server/SoManyFeedsServer/App.fs
+++ somanyfeeds-server/SoManyFeedsServer/App.fs
@@ -2,12 +2,32 @@ module SoManyFeedsServer.App
 
 open System
 open Suave
+open Suave.Filters
+open Suave.Operators
+open Suave.RequestErrors
 open SoManyFeedsServer
 
 
-let homePage (user : Authentication.User) : WebPart =
-    Successful.OK <| String.Format("hello {0}", user.name)
+let private articlesPage (user : Authentication.User) : WebPart =
+    Successful.OK <| String.Format ("Hello {0}, read your articles here.", user.name)
+
+
+let private feedsPage (user : Authentication.User) : WebPart =
+    Successful.OK <| String.Format ("Hello {0}, manage your feeds here.", user.name)
+
+
+let private authenticatedPage (user : Authentication.User) : WebPart =
+    choose [
+        path "/feeds" >=> GET >=> feedsPage user
+        path "/" >=> GET >=> articlesPage user
+
+        GET >=> Files.browseHome
+        NOT_FOUND "not found"
+    ]
 
 
 let handler =
-    Authentication.authenticate homePage
+    choose [
+        Authentication.authenticate authenticatedPage
+        UNAUTHORIZED "unauthorized"
+    ]
