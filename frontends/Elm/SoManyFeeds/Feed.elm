b6044042645a74e549f08b78ae8f7a19e7362f96
Damien Le Berrigaud <damien@itsdamo.com>
2018-12-15 17:05:37 -0600

    Create feed from form.

--- frontends/Elm/SoManyFeeds/Feed.elm
+++ frontends/Elm/SoManyFeeds/Feed.elm
@@ -1,4 +1,8 @@
-module SoManyFeeds.Feed exposing (Feed, FeedType, Json, typeToString, fromJson)
+module SoManyFeeds.Feed exposing (Feed, FeedType(..), Fields, Json, createRequest, emptyFields, fromJson, typeFromString, typeToString)
+
+import Http
+import Json.Decode as Decode
+import Json.Encode as Encode
 
 
 type alias Json =
@@ -17,6 +21,13 @@ type FeedType
 type alias Feed =
     { id : Int
     , feedType : FeedType
+    , name : String
+    , url : String
+    }
+
+
+type alias Fields =
+    { feedType : FeedType
     , name : String
     , url : String
     }
@@ -49,3 +60,39 @@ typeToString value =
 
         Rss ->
             "Rss"
+
+
+emptyFields : Fields
+emptyFields =
+    { feedType = Rss
+    , name = ""
+    , url = ""
+    }
+
+
+decoder : Decode.Decoder Json
+decoder =
+    Decode.map4
+        Json
+        (Decode.field "id" Decode.int)
+        (Decode.field "feedType" Decode.string)
+        (Decode.field "name" Decode.string)
+        (Decode.field "url" Decode.string)
+
+
+fieldsEncoder : Fields -> Encode.Value
+fieldsEncoder fields =
+    Encode.object
+        [ ( "feedType", Encode.string <| typeToString fields.feedType )
+        , ( "name", Encode.string fields.name )
+        , ( "url", Encode.string fields.url )
+        ]
+
+
+createRequest : Fields -> Http.Request Json
+createRequest fields =
+    let
+        body =
+            Http.jsonBody <| fieldsEncoder fields
+    in
+    Http.post "/api/feeds" body decoder
--- frontends/Elm/SoManyFeeds/Manage.elm
+++ frontends/Elm/SoManyFeeds/Manage.elm
@@ -1,8 +1,12 @@
 module SoManyFeeds.Manage exposing (main)
 
 import Browser exposing (Document)
-import Html exposing (Html, a, button, dd, div, dl, dt, form, h1, h2, h3, header, input, label, li, nav, p, section, text, ul)
-import Html.Attributes exposing (class, href, type_)
+import Html exposing (Attribute, Html, a, button, dd, div, dl, dt, form, h1, h2, h3, header, input, label, li, nav, option, p, section, select, text, ul)
+import Html.Attributes exposing (class, disabled, href, selected, target, type_, value)
+import Html.Events exposing (on, onInput, onSubmit, targetValue)
+import Http
+import Json.Decode
+import Result
 import SoManyFeeds.Feed as Feed exposing (Feed)
 
 
@@ -15,17 +19,25 @@ type alias Flags =
 type alias Model =
     { userName : String
     , feeds : List Feed
+    , form : Feed.Fields
+    , formSubmitted : Bool
     }
 
 
 type Msg
-    = None
+    = UpdateFormType String
+    | UpdateFormName String
+    | UpdateFormUrl String
+    | CreateFeed
+    | CreateFeedResult (Result Http.Error Feed.Json)
 
 
 init : Flags -> ( Model, Cmd Msg )
 init flags =
     ( { userName = flags.userName
       , feeds = List.map Feed.fromJson flags.feeds
+      , form = Feed.emptyFields
+      , formSubmitted = False
       }
     , Cmd.none
     )
@@ -44,10 +56,12 @@ feedView feed =
             ]
         , dl []
             [ dt [] [ text "Url" ]
-            , dd [] [ text feed.url ]
+            , dd []
+                [ a [ href feed.url, target "_blank" ] [ text feed.url ]
+                ]
             ]
         , nav []
-            [ button [ class "button secondary" ] [ text "Delete" ]
+            [ button [ class "button secondary" ] [ text "Unsubscribe" ]
             ]
         ]
 
@@ -61,23 +75,50 @@ feedList model =
         ]
 
 
-newFeedForm =
+onSelect : (String -> msg) -> Attribute msg
+onSelect msg =
+    on "change" (Json.Decode.map msg targetValue)
+
+
+newFeedForm : Model -> Html Msg
+newFeedForm model =
+    let
+        feedType =
+            model.form.feedType
+
+        name =
+            model.form.name
+
+        url =
+            model.form.url
+
+        feedTypeOption t =
+            option
+                [ value <| Feed.typeToString t, selected <| feedType == t ]
+                [ text <| Feed.typeToString t ]
+    in
     section []
-        [ form [ class "card" ]
-            [ label []
+        [ form [ class "card", onSubmit CreateFeed ]
+            [ h3 [] [ text "Add a feed" ]
+            , label []
                 [ text "Type"
-                , input [ type_ "text" ] []
+                , div [ class "styled-select" ]
+                    [ select [ onSelect UpdateFormType, disabled model.formSubmitted ]
+                        [ feedTypeOption Feed.Rss
+                        , feedTypeOption Feed.Atom
+                        ]
+                    ]
                 ]
             , label []
                 [ text "Name"
-                , input [ type_ "text" ] []
+                , input [ type_ "text", value name, onInput UpdateFormName, disabled model.formSubmitted ] []
                 ]
             , label []
                 [ text "Url"
-                , input [ type_ "text" ] []
+                , input [ type_ "text", value url, onInput UpdateFormUrl, disabled model.formSubmitted ] []
                 ]
             , nav []
-                [ button [ class "button primary" ] [ text "Create" ]
+                [ button [ class "button primary", disabled model.formSubmitted ] [ text "Subscribe" ]
                 ]
             ]
         ]
@@ -88,19 +129,56 @@ view model =
     { title = "SoManyFeeds - A feed aggregator by Damien Le Berrigaud"
     , body =
         [ header [] [ h1 [] [ text "SoManyFeeds" ] ]
-        , h2 [] [ text "Manage" ]
-        , h1 [] [ text "Feeds" ]
-        , newFeedForm
+        , h2 [] [ text "Feeds" ]
+        , h1 [] [ text "Manage your subscriptions" ]
+        , newFeedForm model
         , feedList model
         ]
     }
 
 
+feedCreated : Model -> Feed.Json -> Model
+feedCreated model json =
+    let
+        feed =
+            Feed.fromJson json
+    in
+    { model
+        | formSubmitted = False
+        , form = Feed.emptyFields
+        , feeds = [ feed ] ++ model.feeds
+    }
+
+
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
+    let
+        form =
+            model.form
+
+        updateForm f =
+            { model | form = f }
+    in
     case msg of
-        None ->
-            ( model, Cmd.none )
+        UpdateFormType value ->
+            ( updateForm { form | feedType = Feed.typeFromString value }, Cmd.none )
+
+        UpdateFormName value ->
+            ( updateForm { form | name = value }, Cmd.none )
+
+        UpdateFormUrl value ->
+            ( updateForm { form | url = value }, Cmd.none )
+
+        CreateFeed ->
+            ( { model | formSubmitted = True }
+            , Http.send CreateFeedResult <| Feed.createRequest form
+            )
+
+        CreateFeedResult (Result.Err err) ->
+            ( { model | formSubmitted = False }, Cmd.none )
+
+        CreateFeedResult (Result.Ok feedJson) ->
+            ( feedCreated model feedJson, Cmd.none )
 
 
 main : Program Flags Model Msg
