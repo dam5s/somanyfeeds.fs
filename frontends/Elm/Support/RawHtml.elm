b3c2901b7411c15cfeff3c312cb2bebc5d046527
Damien Le Berrigaud <damien@itsdamo.com>
2019-01-22 11:58:00 -0700

    Do not even render trackers. Always use SSL.

--- frontends/Elm/Support/RawHtml.elm
+++ frontends/Elm/Support/RawHtml.elm
@@ -1,6 +1,6 @@
 module Support.RawHtml exposing (fromString)
 
-import Html exposing (Attribute, Html)
+import Html exposing (Html)
 import Html.Attributes exposing (target)
 import Html.Parser as Parser
 import VirtualDom
@@ -21,28 +21,64 @@ nodeToHtml node =
         Parser.Text text ->
             Html.text (String.replace "&quot;" "\"" text)
 
-        Parser.Element "a" attrs children ->
-            Html.a
-                (List.map attributeToHtml attrs ++ [ target "_blank" ])
-                (List.map nodeToHtml children)
-
-        Parser.Element "iframe" _ _ ->
-            Html.div [] []
-
-        Parser.Element "embed" _ _ ->
-            Html.div [] []
-
-        Parser.Element "object" _ _ ->
-            Html.div [] []
-
         Parser.Element name attrs children ->
-            VirtualDom.node name
-                (List.map attributeToHtml attrs)
-                (List.map nodeToHtml children)
+            elementToHtml name attrs children
 
         Parser.Comment _ ->
-            Html.div [] []
+            emptyDiv
+
+
+type alias ParserAttribute =
+    ( String, String )
+
+
+elementToHtml : String -> List ParserAttribute -> List Parser.Node -> Html msg
+elementToHtml name attrs children =
+    case isExcluded name attrs of
+        True ->
+            emptyDiv
+
+        False ->
+            case name of
+                "a" ->
+                    Html.a
+                        (List.map attributeToHtml attrs ++ [ target "_blank" ])
+                        (List.map nodeToHtml children)
+
+                _ ->
+                    VirtualDom.node
+                        name
+                        (List.map attributeToHtml attrs)
+                        (List.map nodeToHtml children)
+
+
+excludedTags =
+    [ "iframe", "embed", "object" ]
+
+
+isExcluded : String -> List ParserAttribute -> Bool
+isExcluded tag attrs =
+    let
+        excludedTag =
+            List.member tag excludedTags
+
+        excludedArsTechnicaTracker =
+            List.member ( "class", "feedflare" ) attrs
+
+        slashDotTrackers =
+            List.member ( "class", "share_submission" ) attrs
+    in
+    excludedTag || excludedArsTechnicaTracker || slashDotTrackers
+
+
+emptyDiv =
+    Html.div [] []
 
 
 attributeToHtml ( name, value ) =
-    VirtualDom.attribute name value
+    case name of
+        "src" ->
+            VirtualDom.attribute name (String.replace "http://" "https://" value)
+
+        _ ->
+            VirtualDom.attribute name value
