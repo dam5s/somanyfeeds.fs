34269afd8cd1e08c6f7743ce110c1f5d3b1611fa
Damien Le Berrigaud <damien@itsdamo.com>
2019-01-29 16:18:38 -0600

    Fix cache busting timestamp.

--- build/Program.fs
+++ build/Program.fs
@@ -103,7 +103,7 @@ let private somanyfeedsServerIntegrationTests _ =
 
 
 let private setupCacheBustingLinks _ =
-    let timestamp = (new DateTimeOffset()).ToUnixTimeSeconds().ToString()
+    let timestamp = DateTimeOffset(DateTime.Now).ToUnixTimeSeconds().ToString()
     let timestampPath = "somanyfeeds-server/bin/Release/netcoreapp2.1/publish/Resources/templates/_assets_version.html.liquid"
     File.write false timestampPath [timestamp]
     ()
