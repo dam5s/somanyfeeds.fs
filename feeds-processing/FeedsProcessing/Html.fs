a0d4586e41c7db351a8febac39c65ca377b7000b
Damien LeBerrigaud <git@itsdamo.com>
2021-07-19 19:16:12 -0600

    Remove tracking images from NPR RSS feeds.

--- feeds-processing/FeedsProcessing/Html.fs
+++ feeds-processing/FeedsProcessing/Html.fs
@@ -10,12 +10,21 @@ let private allow attr (s: HtmlSanitizer) =
     s.AllowedAttributes.Add(attr) |> ignore
     s
 
+let private removeUrlsContaining (substring: string) (s: HtmlSanitizer) =
+    s.FilterUrl.Add(fun (e: FilterUrlEventArgs) ->
+        if e.OriginalUrl.Contains(substring)
+        then e.SanitizedUrl <- null
+        else ()
+    )
+    s
+
 let private sanitizer =
     HtmlSanitizer()
     |> allow "class"
     |> disallow "width"
     |> disallow "height"
     |> disallow "style"
+    |> removeUrlsContaining "/tracking"
 
 let sanitize =
     sanitizer.Sanitize
